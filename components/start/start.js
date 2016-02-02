216f57f231e1ba2c24b89da8c9908610387870da
Klejj <kjberglund@gmail.com>
2016-02-02 22:58:44 +0100

    q

--- components/start/start.js
+++ components/start/start.js
@@ -2,6 +2,6 @@ angular.module("start")
     .controller("StartCtrl", ['$scope', function($scope) {
         $scope.go = function(p) {
             $scope.key = p;
-            $location..path("/journey");
+            $location.path("/journey");
         }
 }]);
