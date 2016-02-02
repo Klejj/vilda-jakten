e220cf80edc10f22c8e8c928ffe97b48642cd168
Klejj <kjberglund@gmail.com>
2016-02-03 00:34:21 +0100

    asdasd

--- components/journey/journey.js
+++ components/journey/journey.js
@@ -2,5 +2,5 @@ angular.module("Journey", ['Data'])
     .controller("JourneyCtrl", ['$scope', '$http', 'DataFactory', function($scope, $http, datafactory) {
         var key = datafactory.data.key;
         var data = '{"iv":"KB2CCO2Ya9/tP/f9B0viCg==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"RiEDgwTfG6k=","ct":"itq6wAP5CLUItit/gFLuArnUYSAFOw=="}';
-        var text = sjcl.decrypt(key, data);
+        $scope.text = sjcl.decrypt(key, data);
 }]);
