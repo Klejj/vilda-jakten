3624af30c47a28637dbea8b37ea681886eec5d98
Klejj <kjberglund@gmail.com>
2016-02-03 00:19:41 +0100

    asdasd

--- components/start/start.js
+++ components/start/start.js
@@ -1,7 +1,7 @@
 angular.module("Start", ['Data'])
-    .controller("StartCtrl", ['$scope', '$location', 'Data', function($scope, $location, data) {
+    .controller("StartCtrl", ['$scope', '$location', 'DataFactory', function($scope, $location, data) {
         $scope.go = function(p) {
-            data.DataFactory.setKey(p);
+            data.setKey(p);
             $location.path("/journey");
         }
 }]);
