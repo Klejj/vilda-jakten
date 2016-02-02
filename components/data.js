775748d990d7e0cdbed95da4311d1ced546a2571
Klejj <kjberglund@gmail.com>
2016-02-03 00:29:10 +0100

    asdasd

--- components/data.js
+++ components/data.js
@@ -1,13 +1,8 @@
 angular.module("Data")
 .factory('DataFactory', function() {
-    var data = {
-        key: ''
-    };
     return {
-        getKey: function () {
-            return data.key;
-        },
-        setKey: function (key) {
-            data.key = key}
+        data: {
+            key: ''
+        }
     };
 });
--- components/start/start.js
+++ components/start/start.js
@@ -1,7 +1,7 @@
 angular.module("Start", ['Data'])
     .controller("StartCtrl", ['$scope', '$location', 'DataFactory', function($scope, $location, datafactory) {
         $scope.go = function(p) {
-            datafactory.setKey(p);
+            datafactory.data.key = p;
             $location.path("/journey");
         }
 }]);
