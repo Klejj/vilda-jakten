dc667d147751e11f5a51c52c5026c6499c88f88a
Klejj <kjberglund@gmail.com>
2016-02-02 23:08:07 +0100

    t

--- components/journey/journey.js
+++ components/journey/journey.js
@@ -1,11 +1,5 @@
 angular.module("journey")
     .controller("JourneyCtrl", ['$scope', '$http', function($scope, $http) {
-        var text = "Test";
-
-        $http.get('/journey/data').success(function(data, status, headers, config) {
-            if(data && status === 200) {
-                $scope.data = data;
-                var text = sjcl.decrypt($scope.key, data);
-            }
-        });
+        var data = '{"iv":"FsssZvVQGWxyrEcstHMF3A==", "v":1, "iter":1000, "ks":128, "ts":64, "mode":"ccm", "adata":"", "cipher":"aes", "salt":"XzLHwbYDXoo=", "ct":"0UsfTPdP8A9TByk0j/EqfoztEQKrRg=="}',
+        var text = sjcl.decrypt($scope.key, data);
 }]);
