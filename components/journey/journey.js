35b88616fe7461aa5538b7c7c76e5857b777aeb6
Klejj <kjberglund@gmail.com>
2016-02-03 02:02:51 +0100

    asdasd

--- components/journey/journey.js
+++ components/journey/journey.js
@@ -4,4 +4,9 @@ angular.module("Journey", ['Data'])
         var data = '{"iv":"fMJWzPzkTnMfYLS1RDma/Q==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"74m+06K/U9w=","ct":"nIptY2B5aIODN+Ws7qODGJlF8RAAlBaLXExpVHnyzRyvKbwMmc2mL7DAxlE/2fBbbiz9s71vTqYc/DQDoCZ02ib6hCw="}';
         alert(sjcl.decrypt(key, data));
         $scope.text = sjcl.decrypt(key, data)
+        var step = function(a) {
+            if(a === 'Testing') {
+                alert('test');
+            }
+        }
 }]);
