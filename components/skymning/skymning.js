3ba6b51109e7a2ed4e4ff6123297228e380ef984
Klejj <kjberglund@gmail.com>
2016-02-03 02:24:01 +0100

    impress

--- components/skymning/skymning.js
+++ components/skymning/skymning.js
@@ -1,5 +1,8 @@
 angular.module("Skymning", ['Data'])
-    .controller("SkymningtCtrl", ['$scope', '$location', 'DataFactory', function($scope, $location, datafactory) {
+    .controller("SkymningCtrl", ['$scope', '$location', 'DataFactory', function($scope, $location, datafactory) {
+        var key = datafactory.data.key;
+        var data = '{"iv":"+ON6bgM9yNcSWV5e/YfTuA==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"8E8bJHu5nUQ=","ct":"W/yIrlio45HZU2DsA7KZuysxRVBIkcq07AJMiNDiWEJladO4H7U="}';
+        $scope.text = sjcl.decrypt(key, data);
         $scope.step = function(p) {
             if(_.isEqual(sjcl.hash.sha256.hash(p), [-1687690913, 656052728, 1973603027, 1555569003, 2045191929, -978153975, 272095033, -1413294086])) {
                 datafactory.data.key = p;
