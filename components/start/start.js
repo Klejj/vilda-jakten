964e7d2d64b3839e96a0c1c487920da6475ea93f
Klejj <kjberglund@gmail.com>
2016-02-03 01:53:04 +0100

    asdasd

--- components/start/start.js
+++ components/start/start.js
@@ -1,7 +1,7 @@
 angular.module("Start", ['Data'])
     .controller("StartCtrl", ['$scope', '$location', 'DataFactory', function($scope, $location, datafactory) {
         $scope.go = function(p) {
-            if(sjcl.hash.sha256.hash(p) === [-1687690913, 656052728, 1973603027, 1555569003, 2045191929, -978153975, 272095033, -1413294086]) {
+            if(_isEqual(sjcl.hash.sha256.hash(p),[-1687690913, 656052728, 1973603027, 1555569003, 2045191929, -978153975, 272095033, -1413294086])) {
                 datafactory.data.key = p;
                 $location.path("/journey");
             }
