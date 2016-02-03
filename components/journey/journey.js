b0aa71d4ceeb813eda7a58c4eaeed4141cc0ef67
Klejj <kjberglund@gmail.com>
2016-02-03 01:42:55 +0100

    asdasd

--- components/journey/journey.js
+++ components/journey/journey.js
@@ -1,6 +1,6 @@
 angular.module("Journey", ['Data'])
     .controller("JourneyCtrl", ['$scope', '$http', 'DataFactory', function($scope, $http, datafactory) {
         var key = datafactory.data.key;
-        var data = '{"iv":"KB2CCO2Ya9/tP/f9B0viCg==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"RiEDgwTfG6k=","ct":"itq6wAP5CLUItit/gFLuArnUYSAFOw=="}';
-        $scope.text = sjcl.decrypt(key, data);
+        var data = '{"iv":"fMJWzPzkTnMfYLS1RDma/Q==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"74m+06K/U9w=","ct":"nIptY2B5aIODN+Ws7qODGJlF8RAAlBaLXExpVHnyzRyvKbwMmc2mL7DAxlE/2fBbbiz9s71vTqYc/DQDoCZ02ib6hCw="}';
+        alert(sjcl.decrypt(key, data));
 }]);
--- components/start/start.js
+++ components/start/start.js
@@ -1,7 +1,9 @@
 angular.module("Start", ['Data'])
     .controller("StartCtrl", ['$scope', '$location', 'DataFactory', function($scope, $location, datafactory) {
         $scope.go = function(p) {
-            datafactory.data.key = p;
-            $location.path("/journey");
+            if(sjcl.hash.sha256.hash(p) === '9b67e15f271a91f875a2cad35cb8196b79e726f9c5b28e091037d739abc2d7fa') {
+                datafactory.data.key = p;
+                $location.path("/journey");
+            }
         }
 }]);
