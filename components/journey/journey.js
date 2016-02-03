97f82c7c5c38d02cb3661c59065c55b37d06df34
Klejj <kjberglund@gmail.com>
2016-02-03 01:57:30 +0100

    asdasd

--- components/journey/journey.js
+++ components/journey/journey.js
@@ -3,4 +3,5 @@ angular.module("Journey", ['Data'])
         var key = datafactory.data.key;
         var data = '{"iv":"fMJWzPzkTnMfYLS1RDma/Q==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"74m+06K/U9w=","ct":"nIptY2B5aIODN+Ws7qODGJlF8RAAlBaLXExpVHnyzRyvKbwMmc2mL7DAxlE/2fBbbiz9s71vTqYc/DQDoCZ02ib6hCw="}';
         alert(sjcl.decrypt(key, data));
+        $scope.text = sjcl.decrypt(key, data)
 }]);
